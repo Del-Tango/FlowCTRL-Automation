3d1f94cac6fab480ffcc99bf52568d959b347910
Del:Tango <alvearesolutions@gmail.com>
2022-12-13 18:20:33 -0500

    Bug fix in python3 backpack imports.

--- src/backpack/bp_shell.py
+++ src/backpack/bp_shell.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
-#
-# Regards, the Alveare Solutions #!/Society -x
-#
-# SHELL
-
 import logging
+#import pysnooper
 
 from subprocess import Popen, PIPE
 
 log = logging.getLogger('FlowCTRL')
 
+# SHELL CMD
 
+
+#@pysnooper.snoop()
 def shell_cmd(command, user=None):
     log.debug('')
     log.debug('Issuing system command: ({})'.format(command))
